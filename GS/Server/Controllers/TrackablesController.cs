89acc965d1c7e9e5281803d900d00bc98a04bf70
John Thomas <john.thomas@future-comm.com>
2023-03-16 15:56:17 -0500

    Working Down The Grid For Trackables

---
+++ GS/Server/Controllers/TrackablesController.cs
@@ -0,0 +1,29 @@
+﻿using GS.Server.Data;
+using GS.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GS.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackablesController : ControllerBase
+    {
+        private readonly GSContext _context;
+
+        public TrackablesController(GSContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Trackable>>> GetAllTrackables()
+        {
+            var result = await _context.Trackables
+                .ToListAsync();
+
+            return result;
+        }
+    }
+}
